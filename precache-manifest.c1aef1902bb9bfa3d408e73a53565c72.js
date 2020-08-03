81a1b2b50ba116efc5a25dd78d4f92d57d1109e7
rmariuzzo <rubens@mariuzzo.com>
2020-08-03 05:08:16 +0000

    Deploying to gh-pages from  @ f9ad8c8a62b12e292d862468279f07c40628674b 🚀

---
+++ precache-manifest.c1aef1902bb9bfa3d408e73a53565c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c00968a02a4f8f57a0d8052da6e23d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f584dd459d9bdd1cff",
+    "url": "/static/js/2.edc3dc68.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/static/js/2.edc3dc68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3460c1373b04468ba1af",
+    "url": "/static/js/main.30fa3cea.chunk.js"
+  },
+  {
+    "revision": "9d8b3573a72708c55bda",
+    "url": "/static/js/runtime-main.018974bc.js"
+  }
+]);
