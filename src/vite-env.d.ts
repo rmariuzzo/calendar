d704f39a2ec5a0877c30ff5be7ebb0500c29a769
Rubens Mariuzzo <rubens@mariuzzo.com>
2026-05-16 20:19:16 -0400

    feat: add module declaration for Svelte components in vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,10 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
+declare module '*.svelte' {
+  import type { Component } from 'svelte'
+  const component: Component<Record<string, any>, Record<string, any>>
+  export default component
+}
+
 declare const __APP_VERSION__: string
